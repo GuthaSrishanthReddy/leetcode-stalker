6ba7887f04656fccc73e8acb3beaf1ab008b2f15
GuthaSrishanthReddy <srishanthreddygutha@gmail.com>
2025-07-29 21:08:58 +0530

    username anchor

--- web_scraper/settings.py
+++ web_scraper/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
@@ -124,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
