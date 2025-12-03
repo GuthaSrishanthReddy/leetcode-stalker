de32f12ca2491ea50e5971eaba4a5c594e0012e4
GuthaSrishanthReddy <srishanthreddygutha@gmail.com>
2025-12-03 23:53:54 +0530

    eventlistener in addUser component

--- leetcode-stalker-react/frontend/src/components/AddUser.jsx
+++ leetcode-stalker-react/frontend/src/components/AddUser.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useRef } from "react";
 import "../styles/AddUser.css";
 
 export default function AddUser({ handleAddUser, newUsername, setNewUsername }) {
+  const button_ref = useRef(null)
+
+  function handleEnter(e){
+    if(e.key==="Enter"){
+      button_ref.current.click();
+    }
+  }
+
   return (
     <label className="adduser-wrapper" htmlFor="adduser-input">
       <input
@@ -9,11 +18,12 @@ export default function AddUser({ handleAddUser, newUsername, setNewUsername }) 
         placeholder="LeetCode Username"
         className="adduser-input"
         value={newUsername}
+        onKeyDown={handleEnter}
         onChange={(e) => setNewUsername(e.target.value)}
         required
       />
 
-      <button className="adduser-btn" onClick={handleAddUser}>
+      <button className="adduser-btn" onClick={handleAddUser} ref={button_ref}>
         <div className="btn-inner">
           <span className="btn-text"> Add User</span>
         </div>
