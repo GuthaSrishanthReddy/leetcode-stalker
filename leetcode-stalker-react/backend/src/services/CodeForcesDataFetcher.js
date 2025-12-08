26ccc4cc78401de485753263bcf98944ce73fc48
GuthaSrishanthReddy <srishanthreddygutha@gmail.com>
2025-12-08 23:33:31 +0530

    added complete logic to fetch codeforces data

--- leetcode-stalker-react/backend/src/services/CodeForcesDataFetcher.js
+++ leetcode-stalker-react/backend/src/services/CodeForcesDataFetcher.js
@@ -2,6 +2,35 @@ import axios from "axios";
 
 const CODEFORCES_API = "https://codeforces.com/api";
 
+async function getUserSolvedProblemsWithTags(handle) {
+
+  const submissionsRes = await axios.get(
+    `${CODEFORCES_API}/user.status?handle=${handle}`
+  );
+
+  if (submissionsRes.data.status !== "OK") return {};
+
+  const submissions = submissionsRes.data.result;
+
+  const result = {};
+  const seen = new Set();
+
+  submissions.forEach(sub => {
+    if (sub.verdict === "OK") {
+      const key = `${sub.problem.contestId}-${sub.problem.index}`;
+
+      if (!seen.has(key)) {
+        seen.add(key);
+
+        result[sub.problem.name] = sub.problem.tags;  
+      }
+    }
+  });
+
+  return result;
+}
+
+
 
 export default async function CodeforcesDataFetcher(handle) {
   try {
@@ -16,18 +45,15 @@ export default async function CodeforcesDataFetcher(handle) {
     const rank = user.rank || null;
     const maxRating = user.maxRating || null;
 
-    const submissionsRes = await axios.get(
+    const submissionsCountsRes = await axios.get(
       `${CODEFORCES_API}/user.status?handle=${handle}`
     );
 
-    if (submissionsRes.data.status !== "OK") return null;
+    if (submissionsCountsRes.data.status !== "OK") return null;
 
-    const submissions = submissionsRes.data.result;
+    const submissions = submissionsCountsRes.data.result;
 
     const solvedSet = new Set();
-    let easy = 0,
-      medium = 0,
-      hard = 0;
 
     submissions.forEach((sub) => {
       if (sub.verdict === "OK") {
