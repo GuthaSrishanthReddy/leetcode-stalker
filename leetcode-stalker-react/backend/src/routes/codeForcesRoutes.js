6928e698fc7a65cdf92b2decb53d532230c5ccda
GuthaSrishanthReddy <srishanthreddygutha@gmail.com>
2025-12-11 22:23:15 +0530

    added backend routes for codeforeces dashboard

---
+++ leetcode-stalker-react/backend/src/routes/codeForcesRoutes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import {authenticate} from "../middleware/auth.js"
+import { listProfiles, addProfile, deleteProfile, refreshProfile } from "../controllers/codeForcesController.js"
+
+const router = express.Router()
+router.use(authenticate)
+
+router.get("/home", listProfiles)
+router.post("/add/:username", addProfile)
+router.delete("/delete/:username", deleteProfile)
+router.post("/refresh/:username", refreshProfile)
+
+export default router;
